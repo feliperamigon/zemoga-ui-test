b36532eea462b7c1b536a813a629446923a111d4
RAMID138 <diego.x.ramirez.-nd@disney.com>
2020-05-08 14:02:05 -0500

    Navbar for mobile added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,29 +1,28 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
 /** Shared Components */
-import { AddPeopleComponent } from './add-people/add-people.component';
-import { FooterComponent } from './footer/footer.component';
-import { MainTileComponent } from './main-tile/main-tile.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { SmallAdComponent } from './small-ad/small-ad.component';
-import { SmallTileComponent } from './small-tile/small-tile.component';
-import { IntroductionComponent } from './introduction/introduction.component';
-import { RulingsComponent } from './rulings/rulings.component';
-import { SubmitCharComponent } from './submit-char/submit-char.component';
+import { AddPeopleComponent } from "./add-people/add-people.component";
+import { FooterComponent } from "./footer/footer.component";
+import { MainTileComponent } from "./main-tile/main-tile.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { SmallAdComponent } from "./small-ad/small-ad.component";
+import { SmallTileComponent } from "./small-tile/small-tile.component";
+import { IntroductionComponent } from "./introduction/introduction.component";
+import { RulingsComponent } from "./rulings/rulings.component";
+import { SubmitCharComponent } from "./submit-char/submit-char.component";
 
 /** Shared Services */
-import { SharedService } from './shared.service';
+import { SharedService } from "./shared.service";
 
 /** Pipes */
-import { EscapeHtmlPipe } from './pipes/escape-html.pipe';
+import { EscapeHtmlPipe } from "./pipes/escape-html.pipe";
 
 /** Angular Material */
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatSidenavModule } from "@angular/material/sidenav";
 
-const materialModules = [
-  MatSnackBarModule
-];
+const materialModules = [MatSnackBarModule, MatSidenavModule];
 
 const uiComponents = [
   AddPeopleComponent,
@@ -34,23 +33,13 @@ const uiComponents = [
   SmallTileComponent,
   IntroductionComponent,
   RulingsComponent,
-  SubmitCharComponent
+  SubmitCharComponent,
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    materialModules
-  ],
-  declarations: [
-    uiComponents,
-    EscapeHtmlPipe
-  ],
-  exports: [
-    uiComponents
-  ],
-  providers: [
-    SharedService
-  ]
+  imports: [CommonModule, materialModules],
+  declarations: [uiComponents, EscapeHtmlPipe],
+  exports: [uiComponents],
+  providers: [SharedService],
 })
-export class SharedModule { }
+export class SharedModule {}
